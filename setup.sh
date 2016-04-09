722004a2f0041447ba66a357e125c3099889a026
monokoo <monokoo@users.noreply.github.com>
2016-04-09 12:49:56 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 #安装云监工环境脚本
 #sanzuwu@gamil.com
-#脚本适用ubuntu14、debian8
+#脚本适用centos7
 
 #更新源
 
-sudo apt-get update 
+yum update 
 
-#安装python3.4
+#安装python3.4,可以和python2共存
 
-sudo apt-get install -y python3.4 python3.4-dev 
+#编译需要的一些包，酌情安装
+yum groupinstall "Development tools"
+yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-deve
+ 
+#下载解压并编译
+# 国内源http://mirrors.sohu.com/python/3.4.3/Python-3.4.3.tgz
+wget https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tgz
+tar xf Python-3.4.3.tgz
+cd Python-3.4.3
+./configure --prefix=/usr/local --enable-shared
+make
+#请勿使用make install
+make altinstall
+
+#链接库文件
+echo /usr/local/lib >> /etc/ld.so.conf.d/local.conf
+ldconfig
+
+#重启后生效
+
+
+
+
 
 #安装pip，确保本脚本和get-pip.py 文件在一个文件夹
 BASE_DIR="$( cd "$( dirname "$0"  )" && pwd  )"
